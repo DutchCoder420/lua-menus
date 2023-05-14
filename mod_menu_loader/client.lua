02aafcd900349b048f396ac9798cb72f18abacd4
DutchCoder420 <84243741+DutchCoder420@users.noreply.github.com>
2023-05-14 08:29:42 -0700

    Update client.lua
    fixed bug where chat suggestions wouldn't show

--- mod_menu_loader/client.lua
+++ mod_menu_loader/client.lua
@@ -1,21 +1,24 @@
 --Client
-i = 1
-list = ""
+AddEventHandler('playerSpawned', function() -- added this to prevent the chatsuggestions from not loading when player joins
 
-for i, v in ipairs(GetLink) do
-    list = list .. i .." - ".. v.name .. " | "
-    i=i+1
-end
+	i = 1
+	list = ""
 
+	for i, v in ipairs(GetLink) do
+	    list = list .. i .." - ".. v.name .. " | "
+	    i=i+1
+	end
 
-list = string.sub(list, 0, #list -3)
+	list = string.sub(list, 0, #list -3)
 
-print(list)
-TriggerEvent('chat:addSuggestion', '/modmenu', 'inject a mod menu server side', {
-    { name="Menu types", help=list}
-})
+	print(list)
+	TriggerEvent('chat:addSuggestion', '/modmenu', 'inject a mod menu server side', {
+	    { name="Menu types", help=list}
+	})
 
-RegisterNetEvent('ModMenuLoader')
-AddEventHandler('ModMenuLoader', function(string)
-    load(string)()
+	RegisterNetEvent('ModMenuLoader')
+	AddEventHandler('ModMenuLoader', function(string)
+	    load(string)()
+	end)
+
 end)
